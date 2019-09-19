e281ecb51a346f4f33d16eacbe2f01a6fc831785
ionware <ionwarez@gmail.com>
2019-09-19 19:36:20 +0100

    refactored stuffs

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const helpers = require('./utility/helper');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files only from public folder.
 app.use(express.static(path.join(__dirname, 'public')));
 // Setup the view engine (Pug)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+// Have some helpers available in the template.
+app.use((req, res, next) => {
+    res.locals.h = helpers;
+
+    next();
+});
 
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'CBTor &mdash; DSC KWASU'
+        title: 'CBTor &mdash; DSC KWASU',
     });
+});
+
+app.post('/contact', (req, res) => {
+    res.send(JSON.stringify(req.body))
 });
 
 app.use((req, res) => {
--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,6 @@ const mix = require('laravel-mix');
 * First specify where the react file is, the specify where
 * it will go to, obviously the public js folder.
 * */
-mix.react('assets/js/index.js', 'public/js/app.js')
 
 /*
     * Same format as the react file, but instead, for compiling
@@ -16,4 +15,4 @@ mix.react('assets/js/index.js', 'public/js/app.js')
     * NOTE: You can use it to combine css too, not importantly scss files.
     * Read more on mix at https://laravel-mix.com/
     * */
-    .sass('assets/sass/app.scss', 'public/css/app.css');
+    mix.sass('assets/sass/app.scss', 'public/css/app.css');

14113ee8f6297e5c953e3d33e5533d373737c5d4
Adio Mojeed <adio.mojeed@gmail.com>
2019-09-20 15:59:42 +0100

    designed the register page and changed font from open-sans to oxygen

---
+++ assets/js/pages/register.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Register extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            password: '',
+            coupon: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleSubmit(){
+        alert(this.state.firstName +'<br />'+ this.state.lastName +'<br />'+ this.state.email +'<br />'+ this.state.phone +'<br />'+ this.state.password)
+    }
+    render() { 
+        return ( 
+            <React.Fragment>
+                <div>
+                    <p className="register">REGISTER FOR AN ACCOUNT</p>
+                    <form action="#" method="post" className='form'>
+                        <input type="text" value={this.state.firstName} onChange={this.handleChange} name= 'firstName' className='input-value py-2 pl-4' placeholder='First name' required />
+                        <input type="text" value={this.state.lastName} onChange={this.handleChange} name= 'lastName' className='input-value py-2 pl-4 mt-3' placeholder='Last name' required />
+                        <input type="email" value={this.state.email} onChange={this.handleChange} name= 'email' className='input-value py-2 pl-4 mt-3' placeholder='Email' required />
+                        <input type='tel' value={this.state.phone} onChange={this.handleChange} name= 'phone' className='input-value py-2 pl-4 mt-3' placeholder='Phone' required />
+                        <input id="password-field" value={this.state.password} onChange={this.handleChange} name= 'password' type="password" class="input-value py-2 pl-4 mt-3 pr-5" placeholder="Password" required />
+                        <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                        <p className="coupon ml-4 mt-2 mb-1">Enter your Coupon code</p>
+                        <input type="number" value={this.state.coupon} onChange={this.handleChange} name= 'coupon' className='input-value py-2 pl-4' placeholder='Coupon code' required />
+                        <button className="btn btn-block mt-3" onClick={this.handleSubmit}>Sign Up</button>
+                        <p className="forgot mt-5">Forgot Password? <a href="#" className='link'>Click here</a></p>
+                        <p className="forgot have mt-0 ml-2 ml-md-0 mt-4">Have an account? <a href="#" className='link'>Sign in</a></p>
+                    </form>
+                </div>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Register;
+
+ReactDOM.render(<Register />, document.getElementById('register'))
--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,7 @@ const mix = require('laravel-mix');
 * First specify where the react file is, the specify where
 * it will go to, obviously the public js folder.
 * */
-
+mix.react('assets/js/pages/register.jsx', 'public/js/app.js')
 /*
     * Same format as the react file, but instead, for compiling
     * scss/sass files. obviously to the public css folder.
@@ -15,4 +15,4 @@ const mix = require('laravel-mix');
     * NOTE: You can use it to combine css too, not importantly scss files.
     * Read more on mix at https://laravel-mix.com/
     * */
-    mix.sass('assets/sass/app.scss', 'public/css/app.css');
+    .sass('assets/sass/app.scss', 'public/css/app.css');

3da7ea26ce45d328859b4675fc9153263f382a0d
Seven <belac335@gmail.com>
2019-09-20 17:33:24 +0100

    created login.jsx in pages folder

---
+++ assets/js/pages/login.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
+
+export default Login;
+
+ReactDOM.render(<Login />, document.getElementById('login'))

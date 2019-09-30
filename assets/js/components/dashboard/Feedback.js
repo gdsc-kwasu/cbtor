eb6729c7e91884ee3649694fae7ee835a1c4049d
ionware <ionwarez@gmail.com>
2019-09-30 21:56:09 +0100

    Feedback component design

--- assets/js/components/dashboard/Feedback.js
+++ assets/js/components/dashboard/Feedback.js
@@ -3,8 +3,24 @@ import React from 'react';
 const Feedback = (props) => {
     return (
         <div className="row px-md-3 py-md-3">
-            <div className="col-12 col-md-8 box-shadow py-4">
-                <h2>Drop a Feedback.</h2>
+            <div className="col-12 col-md-8 box-shadow py-5 px-5">
+                <h5>Leave your feedback</h5>
+                <p>
+                    Do you have a complaint, or something you will like us to improve, or you
+                    see some part we could do better? Let us know.
+                </p>
+                <form action="/feedback" method="post">
+                    <div className="form-row">
+                        <div className="form-group col-12">
+                            <textarea name="message" id="message" cols="30" rows="10" className="form-control" />
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-12 text-right">
+                            <button className="btn btn-success px-4 py-2" type="submit">Send</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );

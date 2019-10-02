e1504765038087854261ae27efa85a69db693ed5
Adio Mojeed <adio.mojeed@gmail.com>
2019-10-02 16:05:22 +0100

    designed the mobile view of the sidebar and added notification icon

--- assets/js/components/SideBarTitle.js
+++ assets/js/components/SideBarTitle.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 /*
-*
-* */
+ *
+ * */
 const SideBarTitle = () => {
-    return (
-        <div className="sidebar__padded">
-            <div className="sidebar__divider">
-                <h2 className="sidebar__title">
-                    CBTor
-                </h2>
-            </div>
-        </div>
+    return ( <
+        div className = "sidebar__padded" >
+        <
+        div className = "sidebar__divider" >
+        <
+        h2 className = "sidebar__title" >
+        CBTor <
+        /h2> <
+        /div> <
+        /div>
     );
 };
 

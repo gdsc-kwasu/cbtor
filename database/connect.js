f81f4b47e754c23d9891a74f51b29a600d74ded8
ionware <ionwarez@gmail.com>
2020-02-12 13:54:11 +0100

    fix connection timeout to mongo db connection

--- database/connect.js
+++ database/connect.js
@@ -5,6 +5,9 @@ const connect = (mongodbURL) => {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
+        useFindAndModify: false,
+        socketTimeoutMS: 0,
+        connectTimeoutMS: 0,
     })
         .then(() => console.log('Now connected to MongoDB!'))
         .catch(err => console.error('Something went wrong', err));

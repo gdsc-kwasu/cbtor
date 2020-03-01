d17858c92719c1131ed32fc834e6b760b0bde8b9
ionware <ionwarez@gmail.com>
2020-03-01 14:41:07 +0100

    coupon generation on the front-end (download as PDF)

--- assets/js/pages/admin/Credit.js
+++ assets/js/pages/admin/Credit.js
@@ -1,9 +1,100 @@
 import React from 'react'
+import { shuffle } from 'lodash'
+import PDFLoader from './PDFLoader'
 
-const Credit = () => {
-    return (
-        <h1>Credit</h1>
-    )
+class Credit extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            amount: 3,
+            credit: 300,
+            coupons: null
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+    }
+
+    static randomPin() {
+        const rand = () => {
+            return shuffle(String(Math.floor(Math.random() * 10000)).split(''))
+                .join('')
+        }
+
+        return '----'
+            .split('')
+            .map(rand)
+            .join('-')
+    }
+
+    handleInputChange(value, key) {
+        this.setState({
+            [key]: value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        const { amount, credit } = this.state
+        const coupons = []
+        for (let i = 1; i <= amount; i++) {
+            coupons.push({ 
+                pin: Credit.randomPin(),
+                amount: credit
+            })
+        }
+        this.setState({
+            coupons
+        })
+    }
+
+    render() {
+        const { amount, credit, coupons } = this.state
+        const { handleInputChange } = this
+
+        return (
+            <div className="container mt-4">
+                <div className="row justify-content-md-end border-bottom border-light pb-3">
+                    <div className="col-12 col-md-8 col-lg-6">
+                        <form action="#" className="form-group-sm" onSubmit={this.handleSubmit}>
+                            <h6 className="text-primary small text-uppercase">Generate Coupon</h6>
+                            <div className="form-row">
+                                <div className="form-group col-6">
+                                    <input type="number" 
+                                        className="form-control p-2 small" 
+                                        value={amount} 
+                                        placeholder="How many?" 
+                                        onChange={({ target }) => handleInputChange(target.value, 'amount') } />
+                                </div>
+                                <div className="form-group col-3">
+                                    <input type="number" 
+                                        className="form-control p-2 small" 
+                                        value={credit} 
+                                        placeholder="Credit" 
+                                        onChange={({ target }) => handleInputChange(target.value, 'credit')} />
+                                </div>
+                                <div className="form-group col-3">
+                                    <button className="btn btn-primary p-2 btn-block" type="submit">
+                                        Generate
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="row mt-3">
+                    <div className="col-12">
+                        { 
+                            coupons 
+                            && <PDFLoader credits={coupons} />
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Credit;
